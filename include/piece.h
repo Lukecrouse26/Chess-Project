1af7ad3c0c78d7eeef112a9c96804c793f240e4f
crouse_luke <lukecrouse264@gmail.com>
2025-12-29 10:41:42 -0500

    initialized project

---
+++ include/piece.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+#include "Color.h"
+#include "Position.h"
+
+class Piece {
+public:
+    Color color;
+    std::string name;
+    Position pos;
+
+    //funcitons here
+
+};
