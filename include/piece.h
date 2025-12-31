42ade1c74d93e5f789c5c7fce126309452df3b57
crouse_luke <lukecrouse264@gmail.com>
2025-12-31 12:33:21 -0500

    added piece header

--- include/piece.h
+++ include/piece.h
@@ -10,5 +10,15 @@ public:
     Position pos;
 
     //funcitons here
+    Piece(Color color_, const std::string& name_, Position pos_);
 
+    virtual bool isValidMove(const Position& newPos) const = 0;
+
+    Position getPosition() const { return pos; }
+    Color getColor() const { return color; }
+    std::string getName() const { return name; }
+
+
+
+    virtual ~Piece() = default;
 };
