25d71746456092a362ff0ee7b8363bf48cc5cbfe
crouse_luke <lukecrouse264@gmail.com>
2025-12-31 13:38:21 -0500

    made pawn header file

--- include/pawn.h
+++ include/pawn.h
@@ -3,13 +3,29 @@
 #include <vector>
 
 class Pawn : public Piece {
-public:
+private:
     int enPassantFlag;
     int canMove;
     int canTake;
     int canPromote;
 
-    Pawn()
+public:
 
+    Pawn(char color_, Position startPos);
 
+    void move(const Position& newPos);
+    void take(const Position& takePos);
+    void promote();
+
+    void setEnpassantZero();
+    void setCanTake();
+    void setCanMove();
+    void setCanPromote();
+
+    int getEnpassantVal() const;
+    int getCanTakeVal() const;
+    int getCanMoveVal() const;
+    int getCanPromoteVal() const;
+
+    bool isValidMove(const Position& newPos) const override;
 }
